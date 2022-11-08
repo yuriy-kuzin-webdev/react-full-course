1a304c5be3381f015c6f54ac1ebd634524775020
yuriykuzinpd <yuriy.kuzin.pd@gmail.com>
2022-11-08 15:05:33 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className="app-wrapper" >
+        {/* header */}
+        <header className='header'>
+          <img src='https://png.pngtree.com/element_pic/00/16/07/115783931601b5c.jpg' alt=''/>
         </header>
+
+        {/* nav */}
+        <nav className='nav'>
+          <div><a href='#/'>Profile</a></div>
+          <div><a href='#/'>Messages</a></div>
+          <div><a href='#/'>News</a></div>
+          <div><a href='#/'>Music</a></div>
+          <div><a href='#/'>Settings</a></div>
+        </nav>
+
+        {/* main content */}
+        <div className='content'>
+          <div><img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg' alt=''/></div>
+          <div>ava + desc</div>
+          <div>my posts
+            <div>new post</div>
+            <div>
+              <div>post 1</div>
+              <div>post 2</div>
+            </div>
+          </div>
+
+        </div>
+
+
       </div>
     );
   }
