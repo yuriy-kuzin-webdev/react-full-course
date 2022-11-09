0fb2a3df9be84f529d10794fadd5ec5e4044790f
yuriykuzinpd <yuriy.kuzin.pd@gmail.com>
2022-11-09 13:13:22 +0200

    props practice

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -11,8 +11,8 @@ const MyPosts = () => {
         <button>Add post</button>
       </div>
       <div className={classes.posts}>
-        <Post/>
-        <Post/>
+        <Post message='Hi, how are you'/>
+        <Post message='It is my first post'/>
         <Post/>
       </div>
     </div>
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import classes from "./Post.module.css";
 
-const Post = () => {
+const Post = (props) => {
   return (
     <div className={classes.item}>
       <img
         src="https://pyxis.nymag.com/v1/imgs/51b/28a/622789406b8850203e2637d657d5a0e0c3-avatar-rerelease.rsquare.w700.jpg"
         alt=""
       />
-      post 1
+      {props.message}
       <div>
         <span>Like</span>
       </div>
